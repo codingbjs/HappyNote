3acfc5ccbcdf8992e0e2abdb9a540acf6e2a3437
porpo <porpo@daum.net>
2020-06-24 11:19:42 +0900

    수정 : 소스코드 정리

--- app/src/main/java/com/codingbjs/happynote/MainActivity.java
+++ app/src/main/java/com/codingbjs/happynote/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,14 +19,19 @@ import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseRemoteConfig remoteConfig;
-    long newAppVersion = 0;
-    long toolbarImgCount = 0;
+    long newAppVersion = 1;
+    long toolbarImgCount = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getRemoteConfig();
+    }
+
+
+    private void getRemoteConfig() {
         remoteConfig = FirebaseRemoteConfig.getInstance();
         FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
                 .build();
@@ -38,56 +42,53 @@ public class MainActivity extends AppCompatActivity {
                 .addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
                     @Override
                     public void onComplete(@NonNull Task<Boolean> task) {
-                        checkVersion(task.isSuccessful());
-                        if (task.isSuccessful()) {
-                            Log.e("new_app_version"," = " +
-                                    remoteConfig.getLong("new_app_version"));
-                            Log.e("toolbar_img_count"," = " +
-                                    remoteConfig.getLong("toolbar_img_count"));
-                        }
+                        newAppVersion = remoteConfig.getLong("new_app_version");
+                        toolbarImgCount = remoteConfig.getLong("toolbar_img_count");
+                        checkVersion();
                     }
                 });
     }
 
-    private void checkVersion(boolean successful) {
-        if(successful){
-            newAppVersion = remoteConfig.getLong("new_app_version");
-            toolbarImgCount = remoteConfig.getLong("toolbar_img_count");
+    private void checkVersion() {
+        try {
+            PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
+            long appVersion;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                appVersion = pi.getLongVersionCode();
+            } else {
+                appVersion = pi.versionCode;
+            }
 
-            try {
-                PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
-                long appVersion;
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
-                    appVersion = pi.getLongVersionCode();
-                }else {
-                    appVersion = pi.versionCode;
-                }
+            if (newAppVersion > appVersion) {
+                updateDialog();
+                return;
+            }
 
-                if(newAppVersion > appVersion){
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("업데이트 알림.");
-                    builder.setMessage("최신버전이 등록되었습니다.\n업데이트 하세요.")
-                            .setCancelable(false)
-                            .setPositiveButton("업데이트", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
+            // TODO: 2020-06-24 툴바 이미지 다운로드 메소드 정의
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    private void updateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("업데이트 알림.");
+        builder.setMessage("최신버전이 등록되었습니다.\n업데이트 하세요.")
+                .setCancelable(false)
+                .setPositiveButton("업데이트", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 //                                    Intent intent = new Intent(Intent.ACTION_VIEW);
 //                                    intent.setData(Uri.parse("market://details?id=com.codingbjs.happynote"));
 //                                    startActivity(intent);
-                                    Toast.makeText(getApplicationContext(), "업데이트 버튼 클릭됨.", Toast.LENGTH_SHORT).show();
-                                    dialog.cancel();
-                                }
-                            });
-                    AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
-                }
-
-
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
-        }else {
-
-        }
+                        Toast.makeText(getApplicationContext(), "업데이트 버튼 클릭됨.", Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
